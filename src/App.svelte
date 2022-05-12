3aaa4526d4f02a209304b0cc01c51c8386136414
Vlajd <boriskostadinov2005@gmail.com>
2022-05-12 20:43:49 +0200

    Multiple Languages Implementation

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,35 @@
 <script>
-    import English from "./English.svelte";
+    import { onMount } from 'svelte';
+
+    let Module;
+
+    function loadModule(index, path) {
+        async () => {
+            Module = (await import(path)).default;
+        }
+        let elements = document.getElementsByClassName('langSelect');
+        for (let i = 0; i < elements.length; i++) {
+            if (i == index) {
+                elements[i].classList.add('underlined');
+            }
+            else {
+                elements[i].classList.remove('underlined');
+            }
+        }
+    }
+
+    onMount(async () => {
+        Module = (await import('./English.svelte')).default;
+    });
 </script>
-<div id="Lang"><a id="En">English</a> | <a id="De">Deutsch</a> | <a id="Bg">Български</a></div>
-<English/>
+<div id='Lang'>
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <button class='langSelect underlined' on:click={() => loadModule(0, './English.svelte')}>English</button>
+    |
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <button class='langSelect' on:click={() => loadModule(1, './German.svelte')}>Deutsch</button>
+    |
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <button class='langSelect' on:click={() => loadModule(2, './Bulgarian.svelte')}>Български</button>
+</div>
+<svelte:component this={Module}></svelte:component>
---
+++ src/German.svelte
@@ -0,0 +1,6 @@
+<section id="Start">
+    <div>
+        <h1>Boris Kostadinov</h1>
+        <h5>Multimedia</h5>
+    </div>
+</section>
